92dc603e3d14c6be89d5dc0b1cf1cb13d8c288d0
candybharath <126889025+candybharath@users.noreply.github.com>
2023-07-19 07:57:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "mongodb+srv://YOBOT:YOBOT@cluster0.pcq4zsr.mongodb.net/?retryWrites=true&w=majority")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
